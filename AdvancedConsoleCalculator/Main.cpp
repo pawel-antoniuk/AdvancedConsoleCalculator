d8e76b98698bf0caa8319a3cf0353de3872512e2
pawel-antoniuk <pawelantoniuk@outlook.com>
2014-11-11 23:29:18 +0100

    xxx

--- AdvancedConsoleCalculator/Main.cpp
+++ AdvancedConsoleCalculator/Main.cpp
@@ -4,13 +4,13 @@
 #include <cmath>
 #include <vector>
 #include <functional>
+#include <iomanip>
 #include <Windows.h>
+#include <algorithm>
 
 double to_value(std::string::iterator& begin, const std::string::iterator& end) {
 	auto count = 0;
 	auto dot = -1;
-	auto old = begin;
-
 	auto sign = true;
 	if(*begin == '+')
 		++begin;
@@ -18,7 +18,7 @@ double to_value(std::string::iterator& begin, const std::string::iterator& end) 
 		sign = false;
 		++begin;
 	}
-
+	auto old = begin;
 	for(; begin < end; ++begin) {
 		if(isdigit(*begin))
 			++count;
@@ -27,17 +27,17 @@ double to_value(std::string::iterator& begin, const std::string::iterator& end) 
 		else
 			break;
 	}
-
 	double ret = 0;
 	auto offset = count - (dot == -1 ? count : dot) + 1;
-
 	for(; count > 0; ++old) {
 		if(isdigit(*old))
 			ret += (double) (*old - '0') * pow(10, count-- - offset);
 	}
-
 	return ret * (sign ? 1 : -1);
 }
+
+double _next_value(std::string::iterator& begin, const std::string::iterator& end);
+double _next_value2(std::string::iterator& begin, const std::string::iterator& end);
 
 double _calc(std::string::iterator& begin, const std::string::iterator& end) {
 	std::vector<std::vector<double>> values;
@@ -52,34 +52,47 @@ double _calc(std::string::iterator& begin, const std::string::iterator& end) {
 			++begin;
 			break;
 		} else if(*begin == '^') {
-			if(begin + 1 < end && !values.empty() && !values.back().empty())
-				values.back().back() = std::pow(values.back().back(), *(begin + 1) == '(' ? _calc(++begin, end) : to_value(++begin, end));
-			else
+			if(values.empty())
+				values.push_back(std::vector<double>());
+			if(begin + 1 < end) {
+				if(values.back().empty())
+					values.back().push_back(_next_value(begin, end));
+				else if(begin + 1 < end)
+					values.back().back() = std::pow(values.back().back(), _next_value(begin, end));
+			} else
+				break;
+		} else if(*begin == '%') {
+			if(values.empty())
+				values.push_back(std::vector<double>());
+			if(begin + 1 < end) {
+				if(values.back().empty())
+					values.back().push_back(_next_value(begin, end));
+				else if(begin + 1 < end)
+					values.back().back() = std::fmod(values.back().back(), _next_value(begin, end));
+			} else
 				break;
 		} else if(*begin == '*') {
 			if(begin + 1 < end) {
 				if(values.empty())
 					values.push_back(std::vector<double>());
-				values.back().push_back(*(begin + 1) == '(' ? _calc(++begin, end) : to_value(++begin, end));
+				values.back().push_back(_next_value(begin, end));
 			} else
 				break;
 		} else if(*begin == '/') {
 			if(begin + 1 < end) {
 				if(values.empty())
 					values.push_back(std::vector<double>());
-				values.back().push_back(1 / (*(begin + 1) == '(' ? _calc(++begin, end) : to_value(++begin, end)));
+				values.back().push_back(1 / _next_value(begin, end));
 			} else
 				break;
-		} else if(*begin == '%') {
-			if(begin + 1 < end) {
-				if(values.empty())
-					values.push_back(std::vector<double>());
-				values.back().push_back(std::fmod(values.back().back(), *(begin + 1) == '(' ? _calc(++begin, end) : to_value(++begin, end)));
-			} else
-				break;
-		} else if(*begin == '+' || *begin == '-') {
+		} else if(*begin == '+') {
 			if(begin + 1 < end)
-				values.push_back(std::vector<double>{ *(begin + 1) == '(' ? _calc(++begin, end) : to_value(begin, end) });
+				values.push_back(std::vector<double>{ _next_value2(begin, end) });
+			else
+				break;
+		} else if(*begin == '-') {
+			if(begin + 1 < end)
+				values.push_back(std::vector<double>{ -_next_value2(begin, end) });
 			else
 				break;
 		} else if(isdigit(*begin)) {
@@ -92,11 +105,20 @@ double _calc(std::string::iterator& begin, const std::string::iterator& end) {
 	}
 
 	return std::accumulate(values.begin(), values.end(), 0.0, [](double a, std::vector<double> b)->double {
-		return a + std::accumulate(b.begin(), b.end(), 1.0, std::multiplies<double>());
+		return a + (b.empty() ? 0 : std::accumulate(b.begin(), b.end(), 1.0, std::multiplies<double>()));
 	});
 }
 
+double _next_value(std::string::iterator& begin, const std::string::iterator& end) {
+	return *(begin + 1) == '(' ? _calc(begin+=2, end) : to_value(++begin, end);
+}
+
+double _next_value2(std::string::iterator& begin, const std::string::iterator& end) {
+	return *(begin + 1) == '(' ? _calc(begin += 2, end) : to_value(begin, end);
+}
+
 double calc(std::string math) {
+	math.erase(std::remove_if(math.begin(), math.end(), isspace), math.end());
 	auto begin = math.begin();
 	return _calc(begin, math.end());
 }
@@ -123,6 +145,10 @@ public:
 	static void set_position(short x, short y) {
 		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { x, y });
 	}
+
+	static void set_color(unsigned short color) {
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+	}
 };
 
 int main() {
@@ -137,11 +163,18 @@ int main() {
 			}
 		} else if(buffer.length() < 79) {
 			buffer += chr;
+			if(chr == '+' || chr == '-' || chr == '*' || chr == '/' || chr == '^' || chr == '%')
+				ConsoleHelper::set_color(FOREGROUND_RED | FOREGROUND_INTENSITY);
+			else if(chr == '(' || chr == ')')
+				ConsoleHelper::set_color(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
 			std::cout << chr;
+			ConsoleHelper::set_color(FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
 		}
 		auto pos = ConsoleHelper::get_position();
 		ConsoleHelper::set_position(0, pos.second + 1);
-		std::cout << '=' << calc(buffer) << "        ";
+		ConsoleHelper::set_color(FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+		std::cout << '=' << std::setw(79) << std::setfill(' ') << std::left << calc(buffer);
+		ConsoleHelper::set_color(FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
 		ConsoleHelper::set_position(pos.first, pos.second);
 	}
 }
